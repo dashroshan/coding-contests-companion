a7db5ab71cb8e1d628e4f4890e14f01e439153b1
devsargam <sargampoudel100@gmail.com>
2022-10-06 20:56:10 +0545

    🚸 [perf]: embed for all text messages

--- commands/setup-problem.js
+++ commands/setup-problem.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, SlashCommandRoleOption, SlashCommandChannelOption, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, SlashCommandChannelOption, PermissionFlagsBits } = require('discord.js');
+
+const buildEmbed = require("../utility/buildEmbed");
 
 // setup-problem command for turning problem of the day service on for a server
 module.exports = {
@@ -16,19 +18,22 @@ module.exports = {
 
         // Only the admin can use this command
         if (!interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
-            await interaction.editReply({ content: 'You need to be an **Administrator** to run this command!', ephemeral: true });
+            const embed = buildEmbed('No Permissions', 'You need to be an **Administrator** to run this command!')
+            await interaction.editReply({embeds: [embed]});
             return;
         }
 
         // The bot should be able to send messages in the selected channels
         const channel = interaction.options.getChannel('channel');
         if (!channel.permissionsFor(interaction.client.user).has(PermissionFlagsBits.SendMessages)) {
-            await interaction.editReply({ content: 'The bot needs to have **Send Messages** permission in the selected channel!', ephemeral: true });
+            const embed = buildEmbed('No Permissions', 'The bot needs to have **Send Messages** premission in the selected channel!')
+            await interaction.editReply({embeds: [embed]});
             return;
         }
 
         // Save server and channel data to db and send a confirmation message
         await interaction.client.database.saveProblemChannel(interaction.guildId, channel.id);
-        await interaction.editReply({ content: 'Problem of the day service has been actived for you server!', ephemeral: true });
+        const embed = buildEmbed('Problem Activated', 'Problem of the day service has been actived for you server!')
+        await interaction.editReply({embeds: [embed]});
     },
 };
